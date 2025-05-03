8386034cac50c00fff8b2532c5ee79d982f8c277
NatAn <andrei.v.0204@gmail.com>
2025-05-03 16:06:30 +0300

    Добавлены первые UI тесты

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -59,14 +59,14 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         guard let currentQuestion else { return }
-        changeStateButton(isEnabled: false)
+        changeStateButton(isEnabled:false)
         showAnswerResult(isCorrect: currentQuestion.correctAnswer)
     }
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         guard let currentQuestion else { return }
-        changeStateButton(isEnabled: false)
-        showAnswerResult(isCorrect: currentQuestion.correctAnswer)
+        changeStateButton(isEnabled:false)
+        showAnswerResult(isCorrect: !currentQuestion.correctAnswer)
     }
     
     // MARK: - Private Methods
